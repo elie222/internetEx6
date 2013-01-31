5f0a1cc774ae09f9991f55ab0b7ee20e9df849a4
Eliezer Steinbock <eliesteinbock@gmail.com>
2013-01-31 16:23:48 +0200

    we now delete all cookies when logging out in offline mode

--- www/mail/mail.js
+++ www/mail/mail.js
@@ -243,6 +243,18 @@ function logout() {
 function completeLogout() {
     localStorage.clear();
     window.location.href='logout';
+    deleteAllCookies();
+}
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
 }
 
 function buttonPushed(button, id) {
